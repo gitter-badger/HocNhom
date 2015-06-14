b01adf72ef9dc8daba67580e31ecf396e7d66adc
hoangduc9003 <hoangduc9003@gmail.com>
2015-06-14 10:44:58 +0700

    baitap

---
+++ Homework_Duc/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_Duc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SelectionSort s = new SelectionSort();
+            InsertionSort ins = new InsertionSort();
+            int[] a = { 2, 4, 3, 7, 5, 6 };
+            
+            for (int i = 0; i < a.Length; i++)
+            {
+                ins.SortInsertion(a);
+                  
+                Console.Write(a[i] + ", ");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Homework_Duc/SelectionSort.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_Duc
+{
+    class SelectionSort
+    {
+        public void SortSelection(int[] a)
+        {
+            int temp;
+            for (int i = 0; i < a.Length; i++)
+            {
+                for (int j = i+1; j < a.Length; j++)
+                {
+                    if (a[i] > a[j])
+                    {
+                        temp = a[i];
+                        a[i] = a[j];
+                        a[j] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
